0660865bcb3126b9fe481c2aac588ca3f5ab8c8b
Tiwonge Lwara <43346571+lwara@users.noreply.github.com>
2022-05-09 15:37:43 +0200

    Update push.py

--- push.py
+++ push.py
@@ -5,10 +5,12 @@ import platform
 import subprocess
 import requests as requests
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()  # load .env file
 _PASSWORDS_ = os.environ.get("PASSWORD")
 _ENDPOINT_ = os.environ.get("ENDPOINT")
+print(_ENDPOINT_)
 
 
 def get_xi_data(url):
@@ -24,11 +26,14 @@ def get_xi_data(url):
 
 # Follow the following steps :
 # 1. get data from xi
-site_details = get_xi_data()
+site_details = get_xi_data(_ENDPOINT_)
 # 2. record the length of the file
 length = len(site_details)
 counter = 0  # this is a checker
 checker = True
+
+connection_report = pd.DataFrame(columns=['ip', 'facility', 'username', 'code'])
+failed_report = pd.DataFrame(columns=['ip', 'facility', 'username', 'code'])
 print("Start Service !! ")
 print(f"******** NUMBER OF SITES : {length}")
 
@@ -37,6 +42,7 @@ while counter < length:
     ipaddress = site_details[counter]["fields"]["ip_address"]
     username = site_details[counter]["fields"]["username"]
     name = site_details[counter]["fields"]["name"]
+    district = site_details[counter]["fields"]["name"]
 
     # 4. Check if a site is reachable
     param = '-n' if platform.system().lower() == 'windows' else '-c'
@@ -47,10 +53,9 @@ while counter < length:
     if subprocess.call(['ping', param, '1', ipaddress]) == 0:
         print("*************")
         print(f"step 1 :  {name} :  is REACHABLE)")
-        passwords = ["letmein", "xxr_pq7", "lin1088", "password",
-                     "lin@1088", "czemr2020!", "KSCv4GQ", "Tm3zbk4"]
+
         # 5. Test passwords
-        for each in passwords:
+        for each in _PASSWORDS_:
             if checker:
                 # if connected, then push ssh keys
                 # answer = os.system("ssh-copy-id "+username+"@"+ipaddress+" | echo 'yes \n' ")
@@ -59,13 +64,16 @@ while counter < length:
                 ssh_code = answer
                 if ssh_code == 0:
                     print("SSH KEY ADDED")
-                    with open("pushed.json", "r+") as data:
-                        information = {f"site_name{name}": name}
-                        data.write(json.dumps(information))
-                        data.close()
+                    connection_report = connection_report.append(
+                        {'ip': ipaddress, 'facility': name, 'username': username, 'code': ssh_code}, ignore_index=True)
+
                     checker = False
                 print(f"Returned code : {ssh_code}")
+                failed_report = failed_report.append(
+                    {'ip': ipaddress, 'facility': name, 'username': username, 'code': ssh_code}, ignore_index=True)
         checker = True  # put it ack to default settings
+        connection_report.to_excel(f'Reachable-Report.xlsx', index=False, header=True)
+        failed_report.to_excel(f'Unreachable-Report.xlsx', index=False, header=True)
 
         print("****************************")
     counter += 1
